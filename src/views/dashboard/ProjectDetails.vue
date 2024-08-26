133a9e8673cf766e0a21e23d4531015ff358211a
Priyal Soni <priyal15.soni@gmail.com>
2024-08-26 01:11:58 -0700

    Added ProjectSelector and ProjectDetails cards

--- src/views/dashboard/ProjectDetails.vue
+++ src/views/dashboard/ProjectDetails.vue
@@ -1,18 +1,15 @@
 <script setup>
-import illustrationJohnDark from '@images/cards/illustration-john-dark.png';
-import illustrationJohnLight from '@images/cards/illustration-john-light.png';
 import { useTheme } from 'vuetify';
 
 const { global } = useTheme()
-const illustrationJohn = computed(() => global.name.value === 'dark' ? illustrationJohnDark : illustrationJohnLight)
 </script>
 
 <template>
   <VCard class="text-center text-sm-start" style="height: 400px;">
-    <VRow no-gutters>
+    <VRow no-gutters style="height: 100%;">
       <VCol
         cols="12"
-        sm="8"
+        sm="12"
         order="2"
         order-sm="1"
       >
@@ -22,37 +19,29 @@ const illustrationJohn = computed(() => global.name.value === 'dark' ? illustrat
           </VCardTitle>
         </VCardItem>
 
-        <VCardText>
-          Add in reports
-          <br>
-          status of the project and sponsor
-          <br>
-          <VBtn
-            variant="tonal"
-            class="mt-6"
-            size="small"
-          >
-            View Badges
-          </VBtn>
-        </VCardText>
-      </VCol>
+        <VCardText style="height: 100%;">
+          <!-- Project Name -->
+          <VRow class="mb-1" style="height: 49%;">
+            <VCol cols="12" class="d-flex align-items-center">
+              <strong>About:</strong> &nbsp; [Your About Text Here]
+            </VCol>
+          </VRow>
 
-      <VCol
-        cols="12"
-        sm="4"
-        order="1"
-        order-sm="2"
-        class="text-center"
-      >
-        <!-- Illustration could go here -->
+          <!-- Status -->
+          <VRow class="mb-1" style="height: 14%;">
+            <VCol cols="12" class="d-flex align-items-center">
+              <strong>Status:</strong> &nbsp; [Your Status Here]
+            </VCol>
+          </VRow>
+
+          <!-- Sponsor -->
+          <VRow class="mb-1" style="height: 19%;">
+            <VCol cols="12" class="d-flex align-items-center">
+              <strong>Sponsor:</strong> &nbsp; [Your Sponsor Name Here]
+            </VCol>
+          </VRow>
+        </VCardText>
       </VCol>
     </VRow>
   </VCard>
 </template>
-
-<style lang="scss" scoped>
-.john-illustration {
-  inset-block-end: -0.125rem;
-  inset-inline-end: 3.5rem;
-}
-</style>
--- src/views/dashboard/ProjectSelector.vue
+++ src/views/dashboard/ProjectSelector.vue
@@ -1,20 +1,39 @@
 <script setup>
-import illustrationJohnDark from '@images/cards/illustration-john-dark.png';
-import illustrationJohnLight from '@images/cards/illustration-john-light.png';
 import { useTheme } from 'vuetify';
 
-const { global } = useTheme()
-const illustrationJohn = computed(() => global.name.value === 'dark' ? illustrationJohnDark : illustrationJohnLight)
+const { global } = useTheme();
+
+// Define reactive values for the sliders, checkbox, dropdown, and dates
+const rangeValue = ref([10, 90]); // Range slider value
+const singleValue = ref(50); // Single value slider
+const showRangeSlider = ref(false); // Checkbox state
+const selectedProject = ref(''); // Dropdown selection
+
+// Sample projects for the dropdown
+const projects = ['Abdera', 'CommonsRDF'];
+
+// Date values (dummy values to start)
+const startDate = ref('');
+const endDate = ref('');
+
+// Watch the selected project and update dates based on the selection
+watch(selectedProject, (newProject) => {
+  if (newProject === 'Abdera') {
+    startDate.value = '2006-06-06';
+    endDate.value = '2008-11-08';
+  } else if (newProject === 'CommonsRDF') {
+    startDate.value = '2015-03-06';
+    endDate.value = '2016-11-28';
+  }
+});
 </script>
 
 <template>
-  <VCard class="text-center text-sm-start" style="height: 400px;">
+  <VCard class="text-center text-sm-start" style="height: 450px;">
     <VRow no-gutters>
       <VCol
         cols="12"
-        sm="8"
-        order="2"
-        order-sm="1"
+        sm="12"
       >
         <VCardItem class="pb-3">
           <VCardTitle class="text-primary">
@@ -23,36 +42,66 @@ const illustrationJohn = computed(() => global.name.value === 'dark' ? illustrat
         </VCardItem>
 
         <VCardText>
-          Need to add selection box
-          <br>
-          and range slider
-          <br>
+          <!-- Dropdown for selecting a project -->
+          <VSelect
+            v-model="selectedProject"
+            :items="projects"
+            label="Project"
+            class="mb-4"
+            outlined
+            dense
+          />
+
+          <!-- Checkbox to toggle between single value and range slider -->
+          <VCheckbox
+            v-model="showRangeSlider"
+            label="Enable Range Slider"
+            class="mb-4"
+          />
+
+          <!-- Conditional rendering based on checkbox state -->
+          <VSlider
+            v-if="showRangeSlider"
+            v-model="rangeValue"
+            range
+            :min="0"
+            :max="100"
+            class="mt-4"
+            style="width: calc(100% - 10px); margin-right: 10px;"
+            label="Select Range"
+            ticks="always"
+          />
+
+          <VSlider
+            v-else
+            v-model="singleValue"
+            :min="0"
+            :max="100"
+            class="mt-4"
+            style="width: calc(100% - 10px); margin-right: 10px;"
+            label="Select Month"
+            ticks="always"
+          />
+
+          <!-- Static fields for Start Date and End Date, side by side -->
+          <div class="mt-4 d-flex align-center" style="margin-left: 16px;">
+            <div class="d-flex align-center" style="width: 50%;">
+              <strong>Start Date: </strong>&nbsp;{{ startDate }}
+            </div>
+            <div class="d-flex align-center justify-center" style="width: 50%;">
+              <strong>End Date: </strong>&nbsp;{{ endDate }}
+            </div>
+          </div>
+
           <VBtn
             variant="tonal"
             class="mt-6"
             size="small"
           >
-            View Badges
+            Open Parallel window
           </VBtn>
         </VCardText>
-      </VCol>
-
-      <VCol
-        cols="12"
-        sm="4"
-        order="1"
-        order-sm="2"
-        class="text-center"
-      >
-        <!-- Illustration could go here -->
       </VCol>
     </VRow>
   </VCard>
 </template>
-
-<style lang="scss" scoped>
-.john-illustration {
-  inset-block-end: -0.125rem;
-  inset-inline-end: 3.5rem;
-}
-</style>
